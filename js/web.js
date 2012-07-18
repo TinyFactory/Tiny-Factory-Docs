4507c0dd38e21635ba13865a874b50c5b9763e47
areus <hi@areuswade.com>
2012-07-18 12:42:54 -0700

    General cleanup and permalink fixes

--- js/web.js
+++ js/web.js
@@ -2,11 +2,37 @@ $('a.category').click( function() {
   $('.mobile-band ul').slideToggle(350);
   return false;
 });
-var win = $(window).width();
-var view = 1024
-if (win > view) {
-  $('#sidebar').slimScroll({
-    height: '100%',
-    width: '23%',
-  });
-}
+
+// Duplicate sidebar for conditional loading
+var $sidebar = $('#sidebar');
+var $sidebar_ns = $sidebar.clone();
+
+// Give it a new ID so we know it's loading
+$sidebar_ns.attr({id: "sidebar_ns"});
+
+// Insert duplicate sidebar after original sidebar
+$sidebar.after($sidebar_ns);
+
+// Allow sidebar to scroll without scrollbar
+$sidebar.slimScroll({
+ 	height: '100%',
+ 	width: '23%',
+});
+
+// Add or remove function depending on what
+// width is active.
+$(window).resize(function(){
+	var win = $(window).width();
+	var view = 1024
+	if (win > view) {
+	 	$sidebar.show();
+	 	$sidebar_ns.hide();
+	} else {
+		$sidebar.hide();
+		$sidebar_ns.show();
+	}
+});
+
+// Fire resize event to prevent duplicate
+// sidebars from showing.
+$(window).trigger('resize');
