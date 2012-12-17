0b42f0bee9e6967179d69b1f614dea8870aa1fe4
areus <hi@areuswade.com>
2012-12-17 12:35:24 -0800

    Minor template changes, sidebar cleanup, and IE8 support.

---
+++ _includes/footer.md
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="{{site.url}}/js/masonry.js"></script>
+<script type="text/javascript" src="{{site.url}}/js/respond.min.js"></script>
+<script type="text/javascript" src="{{site.url}}/js/jquery.jscrollpane.min.js"></script>
+<script type="text/javascript" src="{{site.url}}/js/jquery.mousewheel.js"></script>
+<script type="text/javascript" src="{{site.url}}/js/mwheelIntent.js"></script>
+<script type="text/javascript" src="{{site.url}}/js/web.js"></script>
--- _site/js/web.js
+++ _site/js/web.js
@@ -1,27 +1,31 @@
 $('a.category').click( function() {
-  $('.mobile-band ul').slideToggle(350);
-  return false;
+	$('.mobile-band ul').slideToggle(350);
+	return false;
+});
+
+$(function(){
+	$('.archive-list').masonry({
+		itemSelector : '.col',
+		columnWidth : 35
+	});
+	$('#sidebar').jScrollPane();
+});
+
+$(window).resize(function() {
+	$('#sidebar').jScrollPane();
 });
 
 // Duplicate sidebar for conditional loading
 var $sidebar = $('#sidebar');
 var $sidebar_ns = $sidebar.clone();
 
-// Give it a new ID so we know it's loading
+// // Give it a new ID so we know it's loading
 $sidebar_ns.attr({id: "sidebar_ns"});
 
-// Insert duplicate sidebar after original sidebar
+// // Insert duplicate sidebar after original sidebar
 $sidebar.after($sidebar_ns);
 
-// Allow sidebar to scroll without scrollbar
-$sidebar.slimScroll({
- 	height: '100%',
- 	width: '23%',
- 	railVisible: true,
-	alwaysVisible: true
-});
-
-// Add or remove function depending on what
+// // Add or remove function depending on what
 // width is active.
 $(window).resize(function(){
 	var win = $(window).width();
@@ -35,13 +39,6 @@ $(window).resize(function(){
 	}
 });
 
-// Fire resize event to prevent duplicate
-// sidebars from showing.
+// // Fire resize event to prevent duplicate
+// // sidebars from showing.
 $(window).trigger('resize');
-
-$(function(){
-  $('.archive-list').masonry({
-    itemSelector : '.col',
-    columnWidth : 35
-  });
-});
